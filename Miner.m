1bde5d9e520a852784c07b718fad6e281d37e3ef
KEN WANG <61583324+ken0225@users.noreply.github.com>
2020-03-29 15:05:57 +0800

    Add files via upload

---
+++ Miner.m
@@ -0,0 +1,41 @@
+classdef Miner < handle
+    
+    properties
+	blockchain
+	end
+    
+	methods
+	function obj = Miner(blockchain)
+	    obj.blockchain = blockchain;
+	end
+
+	function mine(obj, newData)
+	    % get the last block on the current blockchain
+	    latestBlock = obj.blockchain.getLatest();
+	    % construct a new block
+	    newBlock = Block(latestBlock.index+1,...
+		newData,...
+		latestBlock.selfHash);
+	    % find appropriate selfhash
+	    not_found = true;
+	    iter = 1;
+	    Opt.Method = 'SHA-256';
+	    Opt.Input  = 'ascii';
+        
+        tic
+	    while(not_found)
+		newHash = DataHash([strcat(newBlock.getCombined(), num2str(iter))]);            
+            if(strcmp(newHash(1 : 3), '000'))             
+                newBlock.nonce = iter; % solve violently
+                newBlock.selfHash = newHash; % if the approproate selfhash is found
+                disp(newHash) 
+                obj.blockchain.addBlock(newBlock); % add selfhash to blockchain
+                break
+            end
+		iter = iter + 1;
+        end
+        toc
+        
+	end
+    end
+end
