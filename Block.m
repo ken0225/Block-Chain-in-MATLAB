676b2097311639aadaf13d99f6594513720d2241
KEN WANG <61583324+ken0225@users.noreply.github.com>
2020-03-29 14:45:04 +0800

    Update Block.m

--- Block.m
+++ Block.m
@@ -3,14 +3,14 @@ classdef Block < handle
     properties
 	index                   % index of block
 	data                     % transcation data
-	previousHash     % the previous hash
+	previousHash         % the previous hash
 	selfHash              % current hash
 	nonce                  % random number
     end   
     
     methods    
 	function obj = Block(index, data, previousHash)  
-	    if nargin == 2                                                      % genesis block!  
+	    if nargin == 2           % genesis block!  
 	       obj.index = index ;
 	       obj.data = data ;             
 	    elseif nargin == 3                     
@@ -18,13 +18,13 @@ classdef Block < handle
 	       obj.data = data ;
 	       obj.previousHash = previousHash;
 	    end
-    end
+    	end
     
     % The function below converts all data on the block except 'nonce' and
     % 'selfHash' into characters, which is then used to calculate selfHash.
-    function str = getCombined(obj)  
+   	function str = getCombined(obj)  
 	    str = strcat([num2str(obj.index), obj.previousHash, join(obj.data)]);                                                                                                   
-    end 
+    	end 
        
     end
 end
